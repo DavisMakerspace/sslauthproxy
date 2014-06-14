10c91fc6c51d82674de05e4dca75e221d4a91be6
Braden Pellett <git@dabrado.net>
2014-06-14 11:04:21 -0700

    Assume requirement of having a peer cert

--- lib/sslauthserver.rb
+++ lib/sslauthserver.rb
@@ -1,11 +1,11 @@
 require 'openssl'
 
 class SSLAuthServer < OpenSSL::SSL::SSLServer
-  def initialize(server, server_key, server_cert, client_ids, verify_mode=OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT)
+  def initialize(server, server_key, server_cert, client_ids)
     ssl_context = OpenSSL::SSL::SSLContext.new
     ssl_context.key = server_key
     ssl_context.cert = server_cert
-    ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER | verify_mode
+    ssl_context.verify_mode = OpenSSL::SSL::VERIFY_PEER | OpenSSL::SSL::VERIFY_FAIL_IF_NO_PEER_CERT
     ssl_context.cert_store = OpenSSL::X509::Store.new
     client_ids.keys.each{|cert|ssl_context.cert_store.add_cert(cert)}
     @clients = client_ids.map{|cert,id|[cert.to_pem,id.to_sym]}.to_h
